685786aeae9663fc1f7c7e1a4e8ed8f0858c60cf
Chintan Shah <chintanshah@promactinfo.com>
2014-11-01 04:51:12 +0530

    Added travis.yml and integrated with CodeClimate

--- dist/angular-svg-pan-zoom.js
+++ dist/angular-svg-pan-zoom.js
@@ -20,8 +20,6 @@ var AngularSvgPanZoom;
                 };
                 var panEnabled = _this.CheckForBoolean($attrs.panEnabled, true), controlIconEnabled = _this.CheckForBoolean($attrs.controlIconsEnabled, false), zoomEnabled = _this.CheckForBoolean($attrs.zoomEnabled, true), dblClickZoomEnabled = _this.CheckForBoolean($attrs.dblClickZoomEnabled, true), zoomScaleSensitivity = $attrs.zoomScaleSensitivity || 0.2, minZoom = $attrs.minZoom || 0.5, maxZoom = $attrs.maxZoom || 10, fit = _this.CheckForBoolean($attrs.fit, true), center = _this.CheckForBoolean($attrs.center, true), refreshRate = $attrs.refreshRate || 'auto', beforeZoom = $attrs.beforeZoom || bZoom, onZoom = $attrs.onZoom || oZoom, beforePan = $attrs.beforePan || bPan, onPan = $attrs.onPan || oPan;
 
-                debugger;
-
                 _this.spz($element[0], {
                     panEnabled: panEnabled,
                     controlIconsEnabled: controlIconEnabled,
@@ -41,7 +39,6 @@ var AngularSvgPanZoom;
             };
             this.$rootScope = $rootScope;
         }
-
         SvgPanZoomDirective.prototype.CheckForBoolean = function (value, defaultValue) {
             if (value === undefined)
                 return defaultValue;
--- karma.conf.js
+++ karma.conf.js
@@ -32,14 +32,15 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'src/*.ts': ['typescript']
+            'src/*.ts': ['typescript'],
+            'src/*.ts': ['coverage']
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
@@ -82,6 +83,19 @@ module.exports = function (config) {
             transformPath: function (path) {
                 return path.replace(/\.ts$/, '.js');
             }
-        }
+        },
+
+        coverageReporter: {
+            reporters: [
+                {type: 'lcov', dir: 'test/coverage/'},
+                {type: 'text-summary', dir: 'test/coverage/'}
+            ]
+        },
+
+        plugins: [
+            'karma-jasmine',
+            'karma-phantomjs-launcher',
+            'karma-coverage'
+        ]
     });
 };
